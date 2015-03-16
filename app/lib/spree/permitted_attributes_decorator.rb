6fe127f76dca1f0167d4b5a0fbe4d189c5936ea0
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-03-16 12:09:02 +0700

    1

--- app/lib/spree/permitted_attributes_decorator.rb
+++ app/lib/spree/permitted_attributes_decorator.rb
@@ -1,5 +1,5 @@
 module Spree
   module PermittedAttributes
-  @@user_attributes = [:email, :password, :password_confirmation, :first_name, :last_name, :birthday]
+  @@user_attributes = [:email, :password, :first_name, :last_name, :birthday]
   end
 end

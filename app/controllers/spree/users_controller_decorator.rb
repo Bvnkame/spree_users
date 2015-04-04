2987ad4f8481b79a95dd10c670c177589b1db7e5
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-04-04 12:24:08 +0700

    update

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -17,4 +17,23 @@ Spree::Api::UsersController.class_eval do
 		end
 	end
 
+	def show
+		@user ||= Spree.user_class.accessible_by(current_ability, :read).find(params[:id])
+		render "spree/api/users/show_detail", status: 200
+	end
+
+	def update
+		@user ||= Spree.user_class.accessible_by(current_ability, :read).find(params[:id])
+		authorize! :update, @user
+		if @user.update(user_update_params)
+			render "spree/api/users/show_detail", status: 200
+		else
+			invalid_resource!(@user)
+		end
+	end
+
+	def user_update_params
+		params.require(:user).permit(:first_name, :last_name, :avatar_url, :password, :birthday)
+	end
+
 end
---
+++ app/views/spree/api/users/show_detail.v1.rabl
@@ -0,0 +1,5 @@
+object @user
+
+attributes :email, :first_name, :last_name, :avatar_url, :birthday
+node(:money_account) { "0" }
+node(:currency) { "USD" }

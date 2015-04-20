ab795a433bfd181d824c17b72de795351d881838
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2015-04-20 14:30:31 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Spree::Core::Engine.routes.draw do
   post "/api/logout" => "log_user#logout"
 
 
-	get "/api/users/account" => "api/users#user_account"
+	get "/api/user_account" => "api/users#user_account"
  	post "/api/change_password" => "api/users#change_password"
 end
